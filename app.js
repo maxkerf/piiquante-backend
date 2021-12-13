f065a7839f498c3e96c1a4861780c66facc2b7a1
Maxime Kerfourn <maxime.kerfourn@orange.fr>
2021-12-13 17:31:02 +0100

    add mongoose + connect to MongoDB + correct CORS

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use((req, res) => {
-	console.log("Requête reçue !");
-	res.status(200).json("Votre requête a bien été reçue !");
+mongoose
+	.connect(
+		"mongodb+srv://Max:fVD6eJwL4vXkxccL@piiquante.1hgfn.mongodb.net/piiquante?retryWrites=true&w=majority",
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	)
+	.then(() => console.log("Connexion à MongoDB réussie !"))
+	.catch(() => console.error("Connexion à MongoDB échouée..."));
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, PATCH, OPTIONS"
+	);
+	next();
 });
 
 module.exports = app;
